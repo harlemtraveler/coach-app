22e3b5df83f27a4c7247871be791338696bdda40
Tim <tjk315@gmail.com>
2018-07-29 18:53:39 -0400

    Convert stateful components to functional

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,23 +4,21 @@ import '../../styles/noscript.css';
 import '../../styles/main.css';
 // import './Banner.css';
 
-class Banner extends Component {
-  render () {
-    return (
-      <section id="banner">
-      	<div class="inner">
-      		<h2>Myers Consulting</h2>
-      		<p>Career Coaching Rethought<br />
-      		launch your career<br />
-      		to the next level.</p>
-      		<ul class="actions special">
-      			<li><a href="#" class="button primary">Activate</a></li>
-      		</ul>
-      	</div>
-      	<a href="#one" class="more scrolly">Learn More</a>
-      </section>
-    );
-  }
+function Banner(props) {
+  return (
+    <section id="banner">
+    	<div class="inner">
+    		<h2>Myers Consulting</h2>
+    		<p>Career Coaching Rethought<br />
+    		launch your career<br />
+    		to the next level.</p>
+    		<ul class="actions special">
+    			<li><a href="#" class="button primary">Activate</a></li>
+    		</ul>
+    	</div>
+    	<a href="#one" class="more scrolly">Learn More</a>
+    </section>
+  );
 };
 
 export default Banner;
